d01bae5117ddc2ded3e6e55bf5a4bc2bc415b59e
FrancescoLunesu <francesco.lunesu@outlook.it>
2020-07-22 15:25:23 +0200

    Prove con fadeIn e fadeOut

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // richiamo l'icona "i" con la class "fa-bars" e faccio in modo che al click parta la funzione che mostra la classe CSS ".hamburger-menu"
 $("i.fa-bars").click(function(){
-    $(".hamburger-menu").show("fast");
+    $(".hamburger-menu").fadeIn(500);
 });
 // richiamo l'icona "i" con la class "fa-times" (la X per chiudere la schermata) e faccio in modo che al click parta la funzione che seleziona la classe CSS ".hamburger-menu" e la nasconde (comando .hide())
 $("i.fa-times").click(function(){
-    $(".hamburger-menu").hide("fast");
+    $(".hamburger-menu").fadeOut(500);
 });
